612ff14c9990866efcf975e3688a96c73af539b3
Stefano Kowalke <blueduck@gmx.net>
2014-01-26 12:32:39 +0100

    In order to use the new API method for SQL error messages
    * changed all occurrences of sql_error() to sqlErrorMessage()
    * internal the method sql_error() already uses sqlErrorMessage()
    * add a switch into test methods to detect if we have
    ext:doctrine_dbal installed. I am took this approach I `cause I
    don’t have to integrate the new methods into the old database
    core class
    
    Change-Id: Ie5626cb4b9546927e74765025d79599a2b465c6b

--- typo3/sysext/frontend/Classes/ContentObject/ContentObjectRenderer.php
+++ typo3/sysext/frontend/Classes/ContentObject/ContentObjectRenderer.php
@@ -3491,7 +3491,7 @@ class ContentObjectRenderer {
 		$result = FALSE;
 		$conf['select.']['selectFields'] = 'count(*)';
 		$res = $this->exec_getQuery($conf['table'], $conf['select.']);
-		if ($error = $GLOBALS['TYPO3_DB']->sql_error()) {
+		if ($error = $GLOBALS['TYPO3_DB']->sqlErrorMessage()) {
 			$GLOBALS['TT']->setTSlogMessage($error, 3);
 		} else {
 			$row = $GLOBALS['TYPO3_DB']->sql_fetch_row($res);
@@ -7596,7 +7596,7 @@ class ContentObjectRenderer {
 			// Finding the total number of records, if used:
 			if (strstr(strtolower($conf['begin'] . $conf['max']), 'total')) {
 				$res = $GLOBALS['TYPO3_DB']->exec_SELECTquery('count(*)', $table, $queryParts['WHERE'], $queryParts['GROUPBY']);
-				if ($error = $GLOBALS['TYPO3_DB']->sql_error()) {
+				if ($error = $GLOBALS['TYPO3_DB']->sqlErrorMessage()) {
 					$GLOBALS['TT']->setTSlogMessage($error);
 				} else {
 					$row = $GLOBALS['TYPO3_DB']->sql_fetch_row($res);
@@ -7801,7 +7801,7 @@ class ContentObjectRenderer {
 		$outArr = array();
 		if (is_array($listArr) && count($listArr)) {
 			$res = $GLOBALS['TYPO3_DB']->exec_SELECTquery('uid', 'pages', 'uid IN (' . implode(',', $listArr) . ')' . $this->enableFields('pages') . ' AND doktype NOT IN (' . $this->checkPid_badDoktypeList . ')');
-			if ($error = $GLOBALS['TYPO3_DB']->sql_error()) {
+			if ($error = $GLOBALS['TYPO3_DB']->sqlErrorMessage()) {
 				$GLOBALS['TT']->setTSlogMessage($error . ': ' . $GLOBALS['TYPO3_DB']->debug_lastBuiltQuery, 3);
 			} else {
 				while ($row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res)) {
